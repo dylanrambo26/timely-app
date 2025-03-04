97fd37ff250a8695bf52058a193d7b067a64df3b
dylanrambo26 <DYLANRAMBO.26@GMAIL.COM>
2025-03-03 16:12:12 -0800

    Added Simple Top Bar inside Scaffold

--- app/src/main/java/com/example/timemanagementapp/MainActivity.kt
+++ app/src/main/java/com/example/timemanagementapp/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.timemanagementapp.ui.theme.TimeManagementAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +29,59 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TimeManagementAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TimelyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TimelyAppTextAndImages(
+    modifier: Modifier = Modifier
+) {
+    ScaffoldUI(modifier)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldUI(
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Welcome, User")
+                }
+
+            )
+        },
+    ) {
+        innerPadding ->
+        Column (
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ){
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text= "Test"
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TimelyApp() {
     TimeManagementAppTheme {
-        Greeting("Android")
+        TimelyAppTextAndImages(modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+        )
     }
 }
