f10055cdbd54b63dbcc557f458c9b745cf47b9c1
dylanrambo26 <DYLANRAMBO.26@GMAIL.COM>
2025-03-04 19:07:33 -0800

    Added Row with Today's Goals and Goals placeholders inside column

--- app/src/main/java/com/example/timemanagementapp/MainActivity.kt
+++ app/src/main/java/com/example/timemanagementapp/MainActivity.kt
@@ -18,13 +18,32 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import com.example.timemanagementapp.ui.theme.TimeManagementAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -77,13 +96,27 @@ fun ScaffoldUI(
         innerPadding ->
         Column (
             modifier = Modifier
+                .fillMaxSize()
                 .padding(innerPadding),
             verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ){
-            Text(
-                modifier = Modifier.padding(8.dp),
-                text= "Test"
-            )
+            Row(
+                modifier = Modifier
+                .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ){
+                Text(
+                    modifier = Modifier
+                        .padding(8.dp),
+                    text= "Today's Goals:"
+                )
+                Text(
+                    modifier = Modifier
+                        .padding(8.dp),
+                    text= "Goal1\nGoal2\nGoal3\n"
+                )
+            }
         }
     }
 }
